15b4e5043aabd8c160a071a125ea1d399b31c6db
Luizrpimentel <luizrepimentel@gmail.com>
2022-11-11 00:36:04 -0300

    Adicionando exercícios de Funçao com Automação simples

---
+++ Funções/Automação simples.py
@@ -0,0 +1,19 @@
+" Automação simples que fiz para abrir o WPP WEB e mandar mensagens repetidas para a primeira conversa"
+
+import pyautogui as pg
+import webbrowser as wb
+import time
+
+def automocao():
+    wb.open('https://web.whatsapp.com/')
+    time.sleep(10)
+    pg.click(140,247) # ponto da 1 conversa
+    time.sleep(3)
+    pg.click(607,700) # ponto da 1 conversa
+    for enviar in range(30):
+        pg.write('Teste')
+        pg.press('enter')
+
+    return enviar
+
+print(automocao())
